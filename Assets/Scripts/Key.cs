f9ed097057690749689409d9c273360db499868d
Tobias Heiles <heilest@hochschule-trier.de>
2017-11-18 16:55:21 +0100

    Key added

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Key : MonoBehaviour
+{ 
+    [SerializeField] private Key otherKey;
+    [SerializeField] private Image keyImage;
+    [SerializeField] private Sprite keySprite;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            keyImage.sprite = keySprite;
+            if(otherKey)
+                Destroy(otherKey.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
