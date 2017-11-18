c5893bc93bf2ddd9a3187ff70080e73ce12992a1
Tobias Heiles <heilest@hochschule-trier.de>
2017-11-18 22:36:14 +0100

    Background Final Picture

---
+++ Assets/Scripts/GhostSpawn.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostSpawn : MonoBehaviour
+{
+    public float timeWindow = 4.0f;
+    private float timeLeft;
+
+    public int minTimesTriggered = 3;
+
+    private int currentTimesTriggered;
+
+    private bool currentlyChecking;
+    private bool triggered;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (triggered)
+	        return;
+	    if (currentlyChecking)
+	    {
+	        
+	    }
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (triggered)
+            return;
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+        }
+    }
+}
