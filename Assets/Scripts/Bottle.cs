ff965fe41a355fe74b36026f25f56922e2e34811
Tobias Heiles <heilest@hochschule-trier.de>
2017-11-19 00:56:44 +0100

    Plant added (not working yet)

---
+++ Assets/Scripts/Bottle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bottle : MonoBehaviour
+{
+    public Animator anim;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    anim.speed = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.F))
+	    {
+            anim.SetTrigger("TouchBottle");
+	        anim.speed = 1.0f;
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            var player = other.gameObject.GetComponent<PlayerController>();
+            //player.ReceiveDamage();
+
+            Debug.Log("BOTTLE");
+        }
+    }
+}
---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    private bool m_grown;
+
+    public Sprite grownSprite;
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Grow()
+    {
+        m_grown = true;
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+}
